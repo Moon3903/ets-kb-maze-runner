4b0290c639efb5f923eebc3d589d906d44a6f318
zulfikar.fauzul.akbar <zulfikar.fauzul.akbar@gmail.com>
2021-05-31 21:38:12 +0700

    fix queue and change to manhattan

--- src/kb/NodeComparator.java
+++ src/kb/NodeComparator.java
@@ -6,8 +6,11 @@ public class NodeComparator implements Comparator<Node> {
 
     @Override
     public int compare(Node o1, Node o2) {
-        if (o1.getFCost() < o2.getFCost()) {
+        if (o1.getFCost() > o2.getFCost()) {
             return 1;
+        }
+        else if (o1.getFCost() < o2.getFCost()) {
+            return -1;
         }
         return 0;
     }
